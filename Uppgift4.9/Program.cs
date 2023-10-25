aec53ec94001904244fa192c223b42c29f97174c
Bard1en <greger.grym@gmail.com>
2023-10-25 07:57:44 +0200

    Add project files.

---
+++ Uppgift4.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv sidolängden på triangeln");
+            int side = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < side; i++) 
+            {
+
+                for (int j = 0; j <= i; j++) 
+                {
+                    Console.Write("*");
+                }
+
+                if (i != side - 1) 
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
